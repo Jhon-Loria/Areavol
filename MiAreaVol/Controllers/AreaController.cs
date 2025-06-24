f793ca6f1bb5753c3493620aef8a76a66d03ddc2
AndrewStevenDJ <andrewstevendiazj@gmail.com>
2025-06-23 20:04:33 -0600

    Corrijo errores de controladores y estructura, proyecto compila correctamente

--- MiAreaVol/Controllers/AreaController.cs
+++ MiAreaVol/Controllers/AreaController.cs
@@ -243,23 +243,4 @@ public class AreaController : ControllerBase
         // Implementación o eliminar si no es necesario
         return BadRequest("No implementado");
     }
-}
-
-public class Circulo
-{
-    public int Id { get; set; }
-    public double Radio { get; set; }
-    public double Circunferencia => 2 * Math.PI * Radio;
-    public double Area => Math.PI * Math.Pow(Radio, 2);
-}
-
-namespace MiAreaVol.Models
-{
-    public class Triangulo
-    {
-        public int Id { get; set; }
-        public double Base { get; set; }
-        public double Altura { get; set; }
-        public double Area => (Base * Altura) / 2;
-    }
 } 
--- MiAreaVol/Controllers/TrapecioController.cs
+++ MiAreaVol/Controllers/TrapecioController.cs
@@ -16,8 +16,8 @@ namespace MiAreaVol.Controllers
         [HttpGet] public IActionResult Get() => Ok(_service.GetAll());
         [HttpGet("{id}")] public IActionResult Get(int id)
             => _service.GetById(id) is Trapecio t ? Ok(t) : NotFound();
-        [HttpPost] public IActionResult Post([FromBody] Trapecio t) => Ok(_service.Create(t));
-        [HttpPut("{id}")] public IActionResult Put(int id, [FromBody] Trapecio t)
+        [HttpPost] public IActionResult Post(Trapecio t) => Ok(_service.Create(t));
+        [HttpPut("{id}")] public IActionResult Put(int id, Trapecio t)
             => _service.Update(id, t) ? Ok() : NotFound();
         [HttpDelete("{id}")] public IActionResult Delete(int id)
             => _service.Delete(id) ? Ok() : NotFound();
